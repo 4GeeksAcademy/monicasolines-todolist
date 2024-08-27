e5c81101f95cc2f73f1657464714e33a7af45516
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-08-27 00:59:27 +0000

    final

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -3,35 +3,58 @@ import React, { useState } from "react";
 export const TodoList = () => {
     const [todo, setTodo] = useState('');
     const [todos, setTodos] = useState([])
-
+    const [hovered, setHovered] = useState(null);
 
     const addTodo = () => {
         setTodos([...todos, todo]);
         setTodo('')
     }
 
-    const addX = () => {
-
+    const eliminarLi = (index) => {
+        setTodos(todos.filter((element, i) => i !== index));
     }
 
     return (
         <div className="container">
-            <h1> Todo List</h1>
+            <h1 className="text-light m-3" style={{ fontFamily: 'Raleway, sans-serif', fontWeight: 100 }}> Todo List</h1>
             <div className="todo-list">
-                <ul className="list-group" style={{ listStyleType: 'none' }}>
+                <ul className="list-group" id="ul" style={{ listStyleType: 'none' }}>
                     <li className="list-group-item">
                         <input
                             type="text"
                             className="form-control"
                             id="inputTodo"
-                            placeholder="Escribe para agregar a tu lista"
+                            placeholder={todos.length !== 0 ? "Write to add another one" : "No tasks, add a task!"}
                             value={todo}
                             onChange={event => setTodo(event.target.value)}
-                            onKeyDown={event => { if (event.key === 'Enter') { addTodo(); } }} />
+                            onKeyDown={event => { if (event.key === 'Enter') { addTodo(); } }}
+                            style={{ fontFamily: 'Raleway, sans-serif', fontWeight: 100 }}
+                        />
                     </li>
-                    {todos.map((element) => (
-                        <li className="list-group-item" onMouseOver={<i class="fa-regular fa-trash-can"></i>}> ${element} </li>
+                    {todos.map((element, index) => (
+                        <li
+                            className="list-group-item"
+                            key={index}
+                            onMouseOver={() => setHovered(index)}
+                            onMouseOut={() => setHovered(null)}
+                            style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontFamily: 'Raleway, sans-serif', fontWeight: 100 }}
+                        >
+                            <span> {element} </span>
+                            {hovered === index && (
+                                <i
+                                    className="far fa-trash-alt d-flex justify-content-end text-end align-content-end"
+                                    style={{ marginLeft: '10px', cursor: 'pointer' }}
+                                    onClick={() => eliminarLi(index)}
+                                ></i>
+                            )}
+                        </li>
                     ))}
+                    <li
+                        className="list-group-item"
+                        style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontFamily: 'Raleway, sans-serif', fontWeight: 100 }}
+                    >
+                        {todos.length} items left
+                    </li>
                 </ul>
             </div>
         </div>
